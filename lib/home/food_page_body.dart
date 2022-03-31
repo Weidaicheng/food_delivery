b06f0f09b37abb846048a000deb56fa4e2fa0347
Weidaicheng <wdcdavyc@outlook.com>
2022-03-31 09:50:41 +0800

    popular text

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -41,6 +41,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   Widget build(BuildContext context) {
     return Column(
       children: [
+        // slider section
         Container(
           height: Dimensions.pageView,
           child: PageView.builder(
@@ -50,6 +51,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                 return _buildPageItem(position);
               }),
         ),
+        // dots
         new DotsIndicator(
           dotsCount: 5,
           position: _currPageValue,
@@ -58,6 +60,27 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             size: const Size.square(9.0),
             activeSize: Size(18.0, 9.0),
             activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0))
+          ),
+        ),
+        // popular text
+        SizedBox(height: Dimensions.height_30,),
+        Container(
+          margin: EdgeInsets.only(left: Dimensions.width_30),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              BigText(text: "Popular"),
+              SizedBox(width: Dimensions.width_10,),
+              Container(
+                margin: const EdgeInsets.only(bottom: 3),
+                child: BigText(text: ".", color: Colors.black26,),
+              ),
+              SizedBox(width: Dimensions.width_10,),
+              Container(
+                margin: const EdgeInsets.only(bottom: 2),
+                child: SmallText(text: "Food pairing",),
+              )
+            ],
           ),
         )
       ],
